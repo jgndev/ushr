4516c8eed73d6a18d7c25f9df9e12c8ff91a70a9
Jeremy Novak <olepunchy@gmail.com>
2023-05-17 07:24:18 -0500

    Pre-deployment to AWS

--- src/pages/api/shorten.ts
+++ src/pages/api/shorten.ts
@@ -17,7 +17,6 @@ export default async function handler(req: any, res: any) {
           const response: ShortUrlResponse = {
             shortUrl: shortUrl,
           };
-          // return res.status(200).json({ shortUrl: shortUrl });
           return res.status(200).json(response);
         }
       } catch (error) {
--- src/util/dynamodb.ts
+++ src/util/dynamodb.ts
@@ -6,6 +6,17 @@ import {
   GetCommand,
 } from "@aws-sdk/lib-dynamodb";
 import shortid from "shortid";
+import AWS from "aws-sdk";
+
+const credentials = new AWS.Credentials(
+  process.env.AWS_ACCESS_KEY_ID || "",
+  process.env.AWS_SECRET_ACCESS_KEY || ""
+);
+
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  credentials,
+});
 
 const client = new DynamoDBClient({ region: "us-east-1" });
 const ddbDocClient = DynamoDBDocumentClient.from(client);
